489ff110e2a0f2ae22acce92c86ac50fff0be263
Lance Galletti <lgallett@redhat.com>
2021-11-09 09:59:52 -0500

    Updating: minor update

--- 11-neural-networks/linear_regression.py
+++ 11-neural-networks/linear_regression.py
@@ -6,9 +6,9 @@ from tensorflow.keras import layers
 def custom_activation(x):
     return x**2
 
-
 model = keras.models.Sequential()
-model.add(layers.Dense(1, input_dim=1, activation=custom_activation))
+model.add(layers.Dense(1, input_dim=1, use_bias=False, activation=custom_activation))
+model.add(layers.Dense(1))
 model.compile(loss="mean_squared_error")
 
 log = np.array([1, 4])
@@ -41,12 +41,12 @@ X2 = np.array([np.ones(m),X**2]).T
 beta = np.linalg.inv(X2.T @ X2) @ X2.T @ Y
 yestplot = beta[0]+beta[1]*xplot**2
 
-history = model.fit(X, Y, batch_size=50, epochs=1000)
+history = model.fit(X, Y, batch_size=50, epochs=2000)
 
 fig, (ax1, ax2) = plt.subplots(1, 2)
 ax1.plot(xplot,yestplot,lw=4)
 ax1.scatter(X,Y, color='tab:red', s=100)
-ax1.set_title("Least Squares")
+ax1.set_title("Linear Regression")
 
 meshData = np.linspace(-2,2,50)
 Z = model.predict(meshData)
